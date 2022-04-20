ae927a83dbf9460d8f3b9a0af42fef20e2d1e6e4
sirisha <104041359+alla-sirisha@users.noreply.github.com>
2022-04-19 17:50:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,15 @@ streamlit.title('🥗 Kale ,Spinach & Rocket Smoothie')
 streamlit.title('🐔 Hard-Boiled Free-Range Egg')
 streamlit.title('🥑🍞 Avacado Toast')
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
+# let's put a pick list here so they can pick the fruit they want to include
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+#display the table on the page
+streamlit.dataframe(fruits_to_show)                      
+# New Section To Display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('what fruit would you like information about?','kiwi')
+streamlit.write('the user entered',fruit_choice)
